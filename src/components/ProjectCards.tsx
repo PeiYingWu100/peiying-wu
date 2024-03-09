bdc6ece99cf787d8d821424b82249119f1fb485b
Pei-Ying Wu <peiying.wu100@gmail.com>
2024-03-09 22:38:02 +1100

    Separation of Concerns: for Projects (data) & Project Cards

--- src/components/ProjectCards.tsx
+++ src/components/ProjectCards.tsx
@@ -1,3 +1,4 @@
+import Projects from "../data/projects";
 import ProjectCard from "./ProjectCard";
 
 export interface Project {
@@ -9,37 +10,6 @@ export interface Project {
   demoLink: string;
   IsImgRight?: boolean;
 }
-
-const Projects: Project[] = [
-  {
-    img: "../../public/project_rawg.jpg",
-    title: "Project Title",
-    details:
-      "LoreTipsum dolor sit amet consectetur adipisicing elit. Atque quia iusto laboriosam explicabo ipsa fuga facere eum labore nulla repudiandae rem omnis optio pariatur architecto doloremque, minus est assumenda vitae?",
-    tech: "HTML, CSS, JavaScript, React, Vite",
-    githubLink: "https://rawg-game-hub-ochre.vercel.app/",
-    demoLink: "https://rawg-game-hub-ochre.vercel.app/",
-    IsImgRight: true,
-  },
-  {
-    img: "../../public/project_rawg.jpg",
-    title: "Project Title",
-    details:
-      "LoreTipsum dolor sit amet consectetur adipisicing elit. Atque quia iusto laboriosam explicabo ipsa fuga facere eum labore nulla repudiandae rem omnis optio pariatur architecto doloremque, minus est assumenda vitae?",
-    tech: "HTML, CSS, JavaScript, React, Vite",
-    githubLink: "https://rawg-game-hub-ochre.vercel.app/",
-    demoLink: "https://rawg-game-hub-ochre.vercel.app/",
-  },
-  //   {
-  //     img: "",
-  //     title: "",
-  //     details: "",
-  //     tech: "",
-  //     githubLink: "",
-  //     demoLink: "",
-  //      producIimRright: false,
-  //   },
-];
 
 const ProjectCards = () => {
   return (
---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+import { Project } from "../components/ProjectCards";
+
+const Projects: Project[] = [
+    {
+      img: "../../public/project_rawg.jpg",
+      title: "Project Title",
+      details:
+        "LoreTipsum dolor sit amet consectetur adipisicing elit. Atque quia iusto laboriosam explicabo ipsa fuga facere eum labore nulla repudiandae rem omnis optio pariatur architecto doloremque, minus est assumenda vitae?",
+      tech: "HTML, CSS, JavaScript, React, Vite",
+      githubLink: "https://rawg-game-hub-ochre.vercel.app/",
+      demoLink: "https://rawg-game-hub-ochre.vercel.app/",
+      IsImgRight: true,
+    },
+    {
+      img: "../../public/project_rawg.jpg",
+      title: "Project Title",
+      details:
+        "LoreTipsum dolor sit amet consectetur adipisicing elit. Atque quia iusto laboriosam explicabo ipsa fuga facere eum labore nulla repudiandae rem omnis optio pariatur architecto doloremque, minus est assumenda vitae?",
+      tech: "HTML, CSS, JavaScript, React, Vite",
+      githubLink: "https://rawg-game-hub-ochre.vercel.app/",
+      demoLink: "https://rawg-game-hub-ochre.vercel.app/",
+    },
+    //   {
+    //     img: "",
+    //     title: "",
+    //     details: "",
+    //     tech: "",
+    //     githubLink: "",
+    //     demoLink: "",
+    //      producIimRright: false,
+    //   },
+];
+
+export default Projects;
