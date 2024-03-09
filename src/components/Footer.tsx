0ff97c0760eddc3811cae69878142ef1471c203e
Pei-Ying Wu <peiying.wu100@gmail.com>
2024-03-09 22:30:51 +1100

    Build Footer with Page Links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,36 @@
+import { IoIosArrowDropupCircle } from "react-icons/io";
+import { Link } from "react-scroll";
+import NavList from "./NavList";
+
+const Footer = () => {
+  return (
+    <footer className="bg-violet-400 p-4">
+      <section className="max-w-5xl mx-auto p-4 text-white">
+        <nav className="block space-x-8 text-xl" arial-label="main">
+          <ul className="flex flex-col gap-2 md:flex-row items-center justify-between">
+            <li>
+              <Link
+                spy={true}
+                smooth={true}
+                to="home"
+                offset={-70}
+                className="cursor-pointer"
+              >
+                <IoIosArrowDropupCircle className="animate-bounce w-8 h-8" />
+              </Link>
+            </li>
+            <NavList list_classes={["cursor-pointer"]} onClick={() => null} />
+          </ul>
+        </nav>
+        <p className="text-center mt-4">
+          Developed by React.js with Vite, TypeScript, and Tailwind CSS, and
+          deployed on Vercel.
+          <br />
+          Copyright © 2024.
+        </p>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
