fa3b6c0a3ec3a321bf565b641e53dbfe273082b7
Pei-Ying Wu <peiying.wu100@gmail.com>
2024-03-10 21:13:28 +1100

    Update CSS hover text color

--- src/sections/Footer/FooterLink.tsx
+++ src/sections/Footer/FooterLink.tsx
@@ -9,7 +9,7 @@ const FooterLink = ({ href, children }: Props) => {
   return (
     <a
       href={href}
-      className="text-amber-100 hover:text-lime-200"
+      className="text-amber-100 hover:text-amber-200"
       target="_blank"
     >
       {children}
