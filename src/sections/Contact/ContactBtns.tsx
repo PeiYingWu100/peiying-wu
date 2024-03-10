15e7a2616ce8dce8ed3de72dab54bb6448af7b2b
Pei-Ying Wu <peiying.wu100@gmail.com>
2024-03-10 21:58:50 +1100

    Update CSS class

--- src/sections/Contact/ContactBtns.tsx
+++ src/sections/Contact/ContactBtns.tsx
@@ -5,7 +5,7 @@ const ContactBtns = () => {
   return (
     <p className="my-2 flex justify-center items-center gap-4">
       <a
-        className="rounded-lg py-2 bg-blue-600 text-white p-2 text-lg flex items-center justify-center max-w-fit gap-2 hover:opacity-75"
+        className="rounded-lg bg-blue-600 text-white p-2 text-lg flex items-center justify-center max-w-fit gap-2 hover:opacity-75"
         href="https://www.linkedin.com/in/peiyingwu"
       >
         Contact me from{" "}
@@ -14,7 +14,13 @@ const ContactBtns = () => {
         </span>
       </a>
       <Button
-        cssClasses={["max-w-fit", "flex", "items-center", "justify-center"]}
+        cssClasses={[
+          "max-w-fit",
+          "flex",
+          "items-center",
+          "justify-center",
+          "text-lg",
+        ]}
         href="tel:0414082461"
       >
         Call Me
