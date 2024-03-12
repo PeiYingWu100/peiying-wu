c93b365e83811aca6cce90d6ac76d703aa0d442c
Pei-Ying Wu <peiying.wu100@gmail.com>
2024-03-13 10:33:41 +1100

    Projects: Remove project cards before new projects come in

--- src/sections/Projects/Projects.tsx
+++ src/sections/Projects/Projects.tsx
@@ -1,5 +1,5 @@
 import Container from "../../components/Container";
-import ProjectCards from "./ProjectCards";
+// import ProjectCards from "./ProjectCards";
 
 const Projects = () => {
   return (
@@ -7,10 +7,17 @@ const Projects = () => {
       id="projects"
       bgClasses={["bg-gradient-to-r", "from-white", "to-violet-100"]}
       cssClasses={["w-full"]}
-      title="Projects"
+      // title="Projects"
     >
-      <div className="flex justify-center items-center grow flex-wrap my-5">
-        <ProjectCards />
+      <div className="flex justify-around items-center grow flex-wrap my-5 flex-col h-44">
+        <h2 className="text-3xl font-bold text-center text-violet-400">
+          Projects
+        </h2>
+        <p className="text-2xl font-bold text-slate-700 text-center">
+          Exciting things are on the way!
+          <br /> Keep an eye out for new projects coming soon! 🤩
+        </p>
+        {/* <ProjectCards /> */}
       </div>
     </Container>
   );
