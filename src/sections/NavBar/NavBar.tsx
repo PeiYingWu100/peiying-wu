38c479d05191af1f504042633142784fc785906e
PeiYingWu100 <peiying.wu100@gmail.com>
2024-03-16 11:52:45 +1100

    Nav Bar: Make a switching Nav bar Title between English and Mandarin

--- src/sections/NavBar/NavBar.tsx
+++ src/sections/NavBar/NavBar.tsx
@@ -1,15 +1,45 @@
 import { Link } from "react-scroll";
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [isEnText, setIsEnText] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsEnText((prev) => !prev);
+    }, 3000);
+
+    return () => clearInterval(interval); // Clear the interval on component unmount
+  }, []);
+
   return (
     <>
       <header className="sticky top-0 z-30 bg-white">
         <section className="max-w-5xl mx-auto p-4 flex justify-between items-center">
-          <h2 className="text-3xl font-medium">
-            <Link spy={true} smooth={true} to="home" offset={-70}>
-              你好! Welcome!
+          <h2 className="text-2xl font-medium w-56 h-8 inline-block">
+            <Link
+              spy={true}
+              smooth={true}
+              to="home"
+              offset={-70}
+              className="relative"
+            >
+              <span
+                className={`absolute transition-opacity duration-500 w-56 ${
+                  isEnText ? "opacity-100" : "opacity-0"
+                }`}
+              >
+                Welcome! 你好!
+              </span>
+              <span
+                className={`absolute transition-opacity duration-500 w-56 ${
+                  isEnText ? "opacity-0" : "opacity-100"
+                }`}
+              >
+                Welcome! Hello!
+              </span>
             </Link>
           </h2>
 
